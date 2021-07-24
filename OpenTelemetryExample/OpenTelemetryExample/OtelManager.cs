f9533a1c1e7f12cbcbd2b72f74b03ca52018dd45
Vishal Vyas <vishal.vyas@o9solutions.com>
2021-07-24 11:57:55 +0530

    First commit

---
+++ OpenTelemetryExample/OpenTelemetryExample/OtelManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+
+namespace OpenTelemetryExample
+{
+    public class OtelManager
+    {
+        public static void StartOpenTelemetry()
+        {
+            try
+            {
+                StreamReader r = new StreamReader("TracerConfig.json");
+
+                var otConfig = JsonConvert.DeserializeObject<OtConfig>(r.ReadToEnd());
+
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+                var tracerProviderBuilder = Sdk.CreateTracerProviderBuilder().AddAspNetInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddSource(otConfig.OtSource)
+                    .SetSampler(new AlwaysOnSampler())
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(otConfig.OtService))
+                    .AddJaegerExporter();
+                tracerProviderBuilder.Build();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine($"Encountered an error while initializing OpenTelemtry Configuration with message with message [{e.Message}]");
+            }
+
+            
+        }
+
+        public class OtConfig
+        {
+            public string OtSource { get; set; }
+
+            public string OtService { get; set; }
+        }
+
+    }
+}
---
+++ OpenTelemetryExample/OpenTelemetryExample/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetryExample
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+
+            OtelManager.StartOpenTelemetry();
+
+            var repositories = await ProcessRepositories();
+
+            
+            if (repositories != null)
+            {
+                foreach (var repo in repositories)
+                {
+                    Console.WriteLine(repo.Name);
+                    Console.WriteLine(repo.Description);
+                    Console.WriteLine(repo.GitHubHomeUrl);
+                    Console.WriteLine(repo.Homepage);
+                    Console.WriteLine(repo.Watchers);
+                    Console.WriteLine(repo.LastPush);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            try
+
+            {
+                var sourceName = new OtelManager.OtConfig();
+
+                var source = new ActivitySource(sourceName.OtSource);
+                var span = source.StartActivity("NewHttpCall", kind: ActivityKind.Internal);
+
+                span.SetTag("http.customtag", "this is custome tag");
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+                var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+                var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+                span.Stop();
+                return repositories;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            
+        }
+    }
+}
